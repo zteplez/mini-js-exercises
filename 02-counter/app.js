e6def598d1f789d7dffb045d07d26da3babb00c1
zteplez <asibrahim98@gmail.com>
2025-05-02 00:19:52 +0300

    [feat]: Add button event listeners and check status func

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -1,1 +1,34 @@
-const
+const counterDiv = document.querySelector(".counter-value");
+const decreaseBtn = document.querySelector(".decrease");
+const resetBtn = document.querySelector(".reset");
+const increaseBtn = document.querySelector(".increase");
+
+let counter =  0;
+
+decreaseBtn.addEventListener("click", () => {
+    counter--;
+    counterDiv.innerText = counter;
+    checkStatus();
+});
+increaseBtn.addEventListener("click", () => {
+    counter++;
+    counterDiv.innerText = counter;
+    checkStatus();
+});
+resetBtn.addEventListener("click", () => {
+    counter = 0; 
+    counterDiv.innerText = counter;
+    checkStatus();
+});
+
+function checkStatus(){
+    if(counter > 0){
+        counterDiv.style.color = "green";
+
+    }else if(counter < 0){
+        counterDiv.style.color = "red";
+
+    }else{
+        counterDiv.style.color = "gray";
+    }
+}
