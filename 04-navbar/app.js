e253ca120cec546d22be2ab163794ce61ed5ff32
zteplez <asibrahim98@gmail.com>
2025-05-06 21:57:06 +0300

    [feat]: Implement eventListener for hamburger
    * Every click hamburger toggles list
    * If window width wider than 800 px automatically toggles list

--- 04-navbar/app.js
+++ 04-navbar/app.js
@@ -1,10 +1,11 @@
+const hamburgerBtn = document.querySelector(".hamburger");
 
 window.addEventListener('resize', () => {
     if (window.innerWidth < 800) {
         const hamburger = document.querySelector(".hamburger");
         const navMain = document.querySelector(".nav-main");
         const socialMediaGroup = document.querySelector(".social-media-group");
-
+        
         if(hamburger.classList.contains("hide")){
             hamburger.classList.remove("hide");
         }
@@ -18,6 +19,10 @@ window.addEventListener('resize', () => {
         const hamburger = document.querySelector(".hamburger");
         const navMain = document.querySelector(".nav-main");
         const socialMediaGroup = document.querySelector(".social-media-group");
+        const list = document.querySelector(".list-style");
+        if(!list.classList.contains("hide")){
+            list.classList.add("hide");
+        }
 
         if(!hamburger.classList.contains("hide")){
             hamburger.classList.add("hide");
@@ -28,5 +33,14 @@ window.addEventListener('resize', () => {
         if(socialMediaGroup.classList.contains("hide")){
             socialMediaGroup.classList.remove("hide");
         }
+    }
+});
+
+hamburgerBtn.addEventListener("click", ()=>{
+    const list = document.querySelector(".list-style");
+    if(!list.classList.contains("hide")){
+        list.classList.add("hide");
+    }else{
+        list.classList.remove("hide");
     }
 });
